8e883c83098d2406bfae36bd5e00effb18a58c88
choidy180 <choidy180@naver.com>
2022-02-15 02:37:50 +0900

    first_upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+// Animation and Pseudo Selectors
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${( props ) => props.theme.backgroundColor}
+`;
+
+const roatationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    
+  }
+  50% {
+    transform: rotate(180deg);
+    border-radius: 50%;
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`
+const Emoji = styled.span`
+  font-size: 36px;
+`
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 200px;
+  height: 200px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${roatationAnimation} 1s linear infinite;
+  // 스타일 컴포넌트 타겟팅
+  ${Emoji}:hover{
+    font-size: 60px
+  }
+`;
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111"
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Wrapper>
+    {/* <Box bgColor="tomato">
+      <Emoji as="div">🤔</Emoji>
+    </Box> */}
+    <Title>Hello</Title>
+  </Wrapper>
   );
 }
 
